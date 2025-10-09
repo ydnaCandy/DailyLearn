e30652507cdfc2da1c8b96214044de679ae685d2
ydnaCandy <keynomori@gmail.com>
2025-10-09 21:52:55 +0900

    js: consoleへの出力方法

---
+++ js/show_console.js
@@ -0,0 +1,24 @@
+// consoleへの出力
+console.log("hello world")
+
+// 定数
+const greeting = "こんにちは"; 
+
+// 変数
+let score = 100;
+
+console.log(greeting);
+console.log(score);
+
+// letは変数を上書きできる
+score = 150;
+console.log(score);
+
+
+// 型宣言はない
+const item = "banana";
+const price = 100;
+const isAvailable = true;
+
+// 文字列と数値を結合
+console.log(item + "は" + price + "です")
