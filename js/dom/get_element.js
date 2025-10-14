6b1ff986dd27cc7ee9fe6df5dee7f4fa9770958f
ydnaCandy <keynomori@gmail.com>
2025-10-14 22:31:50 +0900

    js: domの基本

---
+++ js/dom/get_element.js
@@ -0,0 +1,8 @@
+
+// 要素の取得
+const testA = document.getElementById("target-text")
+console.log(testA)
+console.log(testA.textContent)
+console.log(testA.id)
+console.log(testA.getAttribute('id'))
+console.log(testA.getAttribute('textContent'))
