fdba1a52faa50188747b808d26f09e67316dc4cd
ydnaCandy <keynomori@gmail.com>
2025-10-15 21:53:19 +0900

    js: domの操作

---
+++ js/dom/control_element.js
@@ -0,0 +1,4 @@
+const targetElement = document.getElementById('target-text');
+
+targetElement.style.color = 'red'
+targetElement.style.backgroundColor = 'blue'
--- js/dom/get_element.js
+++ js/dom/get_element.js
@@ -5,4 +5,9 @@ console.log(testA)
 console.log(testA.textContent)
 console.log(testA.id)
 console.log(testA.getAttribute('id'))
-console.log(testA.getAttribute('textContent'))
+
+
+
+console.log("--- 実行2: 書き換え後 ---");
+console.log("属性 (Attribute):", document.getElementById('myInput').getAttribute('value'));
+console.log("プロパティ (Property):", document.getElementById('myInput').value);
