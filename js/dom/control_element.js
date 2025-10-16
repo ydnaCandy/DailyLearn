6b61e241eae2056c9051ad7d3958a32db62e0ecc
ydnaCandy <keynomori@gmail.com>
2025-10-16 21:57:17 +0900

    feat: テキストの書き込み

--- js/dom/control_element.js
+++ js/dom/control_element.js
@@ -1,4 +1,9 @@
+// 要素の取得
 const targetElement = document.getElementById('target-text');
 
+// 文字色、背景色の変更
 targetElement.style.color = 'red'
 targetElement.style.backgroundColor = 'blue'
+
+// 書き込み
+targetElement.textContent = "書き換えたよ"
