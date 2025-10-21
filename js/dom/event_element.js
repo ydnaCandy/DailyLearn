142e3b5474bcab015ea3daab2edb6d69f548333c
ydnaCandy <keynomori@gmail.com>
2025-10-21 21:17:19 +0900

    js: class要素の操作

--- js/dom/event_element.js
+++ js/dom/event_element.js
@@ -1,9 +1,16 @@
 // ボタン要素の取得
 const button = document.getElementById('change-button');
-
 const targetElement = document.getElementById('target-text');
+const listItems = document.querySelectorAll(".list-item")
 
 // クリック時に実行
 button.addEventListener('click', function() {
     targetElement.style.backgroundColor = 'pink'
+    targetElement.textContent = 'クリックされました'
+    console.log(listItems)
+    // リストに色付け
+    listItems.forEach(function(item) {
+       item.style.color = "pink"
+       item.textContent += ' (色変更済)'
+    });
 });
